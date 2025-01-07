034e12f5fe10a9465e7cc37cad7e1a2e1fb38571
Ryan Zeroual <Zeroualryan@gmail.com>
2025-01-06 21:47:21 -0500

    Added new Task class and Constructors

--- tasktracker/src/main/java/task/tracker/cli/App.java
+++ tasktracker/src/main/java/task/tracker/cli/App.java
@@ -1,16 +1,13 @@
 package task.tracker.cli;
 
-import java.util.Scanner;
 public class App {
     public static void main( String[] args ){
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Choose a number: ");
-        int nombre = scanner.nextInt();
-        int otherNumber = 5;
-        int result = nombre + otherNumber;
-        System.out.println("Result: " + result);
+    Task task = new Task("Task 1");
+    System.out.println(task.toString());
 
-        scanner.close();
-        
+
+
+
+
     }
 }
---
+++ tasktracker/src/main/java/task/tracker/cli/Task.java
@@ -0,0 +1,52 @@
+package task.tracker.cli;
+
+import java.time.LocalDate;
+public class Task {
+    private String description;
+    private int id;
+    private String createDate;
+    private String updateDate;
+
+    public Task(String description){
+        LocalDate now = LocalDate.now();
+        this.description=description;
+        this.id=id;
+        this.createDate=now.toString();
+        this.updateDate=updateDate;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+    
+    public int getId(){
+        return id;
+    }
+
+
+    public String getCreationDate(){
+        return createDate;
+    }
+    public String getUpdateDate(){
+        return updateDate;
+    }
+
+
+
+    public String toString(){  
+        return "Task: "+description+" ID: "+id+" Creation date: "+createDate+" Update date: "+updateDate;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
